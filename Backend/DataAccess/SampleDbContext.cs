c4b170ced93a7fdfe6330c2cdf64e88daeab04a6
Brian Chick <bchick@aegispremier.com>
2019-07-08 00:14:01 -0600

    Added comment to explain why connection string is specified in context.

--- Backend/DataAccess/SampleDbContext.cs
+++ Backend/DataAccess/SampleDbContext.cs
@@ -11,6 +11,8 @@ namespace Backend.DataAccess
 	{
 		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 		{
+			// don't like this but don't want to go down the .NET Core configuration rabbit hole.
+			// since i wanted to test data access from a class library best to but connection string here.
 			optionsBuilder.UseSqlite("Data Source=C:/data/sample.db");
 		}
 
