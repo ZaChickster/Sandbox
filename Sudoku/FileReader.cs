21566e54af99738922a203c35118f3b3261cfd17
ZaChickster <btctech@gmail.com>
2020-04-22 19:33:42 -0600

    Test invalid cases.

--- Sudoku/FileReader.cs
+++ Sudoku/FileReader.cs
@@ -7,6 +7,9 @@ namespace Sudoku
 {
     public class FileReader
     {
+        public const string WRONG_LENGTH = "invalid row size";
+        public const string NON_NUMERIC = "row contains non-numeric chars";
+
         public string FileName { get; set; }
 
         public List<int> ParseRow(string row)
@@ -14,11 +17,11 @@ namespace Sudoku
             char[] onlyNumbers = row.Where(c => char.IsDigit(c)).ToArray();
             char[] nowhitespace = row.Where(c => !char.IsWhiteSpace(c)).ToArray();
 
-            if (onlyNumbers.Length != 9)
-                throw new Exception("invalid row size");
-
             if (new string(onlyNumbers) != new string(nowhitespace))
-                throw new Exception("row contains non-numeric chars");
+                throw new Exception(NON_NUMERIC);
+
+            if (onlyNumbers.Length != 9)
+                throw new Exception(WRONG_LENGTH);
 
             return onlyNumbers.Select(c => int.Parse(c.ToString())).ToList();
         }
