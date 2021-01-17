a5e629f7b40df913d6302809f111f63eb00a7449
Brian Chick <btctech@gmail.com>
2021-01-16 21:24:25 -0700

    Added simple console project that will act as as the device.

---
+++ ConsoleDevice/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleDevice
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Please Enter Device ID:");
+
+			string deviceID = Console.ReadLine();
+
+			Console.WriteLine($"DeviceID {deviceID} was entered.  Press any key to continue ...");
+
+			Console.ReadLine();
+		}
+	}
+}
